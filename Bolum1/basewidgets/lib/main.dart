d10d3eb224681b15d49eb2562f273bcef875e900
uluibrahim <iulu17@posta.pau.edu.tr>
2021-06-24 15:45:03 +0300

    FloatingActionButton ve Material Design

--- Bolum1/basewidgets/lib/main.dart
+++ Bolum1/basewidgets/lib/main.dart
@@ -8,10 +8,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.teal, accentColor: Colors.orange),
       home: Scaffold(
         appBar: AppBar(
           title: Text("Appbar"), // appbar a text basar
-          backgroundColor: Colors.orange, // appbarın arka plan rengi
+          //backgroundColor: Colors.orange, // appbarın arka plan rengi
+          //NOT: Burada appbar rengi tanımlanırsa Theme kısmında tanımlanan primaryColor ezilmiş (override) olur
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // buton tıklanıldığında tetiklenen metot
+            print("Fab tıklanıldı");
+          },
+          child: Icon(
+            Icons.account_balance,
+            color: Colors.white,
+          ),
+        ),
+        body: Text(
+          "Uygulamanın hayat bulduğu appbarın altıdır",
+          style: TextStyle(
+            color: Colors.red, // yazı rengi
+            fontSize: 30, // yazı boyutu
+            fontWeight: FontWeight.bold, // yazı kalınlığı
+          ),
         ),
       ),
       title: "Base widget",
