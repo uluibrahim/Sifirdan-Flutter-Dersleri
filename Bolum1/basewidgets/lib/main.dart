537fa874e091f9753bf8af8d3bf8243b0584c6f7
uluibrahim <iulu17@posta.pau.edu.tr>
2021-06-24 16:29:14 +0300

    container kullanımında padding ve margin

--- Bolum1/basewidgets/lib/main.dart
+++ Bolum1/basewidgets/lib/main.dart
@@ -10,37 +10,58 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.teal, accentColor: Colors.orange),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Appbar"), // appbar a text basar
-            //backgroundColor: Colors.orange, // appbarın arka plan rengi
-            //NOT: Burada appbar rengi tanımlanırsa Theme kısmında tanımlanan primaryColor ezilmiş (override) olur
+        appBar: AppBar(
+          title: Text("Appbar"), // appbar a text basar
+          //backgroundColor: Colors.orange, // appbarın arka plan rengi
+          //NOT: Burada appbar rengi tanımlanırsa Theme kısmında tanımlanan primaryColor ezilmiş (override) olur
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // buton tıklanıldığında tetiklenen metot
+            print("Fab tıklanıldı");
+          },
+          child: Icon(
+            Icons.account_balance,
+            color: Colors.white,
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              // buton tıklanıldığında tetiklenen metot
-              print("Fab tıklanıldı");
-            },
-            child: Icon(
-              Icons.account_balance,
-              color: Colors.white,
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            height: 200,
+            width: 200,
+            color: Colors.red,
+            child: Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.all(20),
+              color: Colors.blue,
+              height: 50,
+              width: 50,
+              child: Text("Emre"),
             ),
           ),
-          body: Container(
-            // container parent'ında bir kutu oluşturur
-            // container default olarak çocuğu kadar yer kaplar
+        ),
 
-            color: Colors.cyan,
-            /**
-              width: 300,
-              height: 300,
-            */
+        /**
+         * BODY
+         * body: Container(
+          // container parent'ında bir kutu oluşturur
+          // container default olarak çocuğu kadar yer kaplar
+
+          color: Colors.cyan,
           
-            // container içinde aligment kullanılır ve width heigth değerleri yok ise parentı kadar alanı kaplar
-            alignment: Alignment.center, 
-            child: Text("BODY CONTAINER"),
-            //  container boyutlarının sınırlandırılması => constraints
-            constraints: BoxConstraints(maxHeight: 200,maxWidth: 200,minHeight: 100,minWidth: 100),
-          )),
+          //    width: 300,
+          //    height: 300,
+          
+          // container içinde aligment kullanılır ve width heigth değerleri yok ise parentı kadar alanı kaplar
+          alignment: Alignment.center,
+          child: Text("BODY CONTAINER"),
+          //  container boyutlarının sınırlandırılması => constraints
+          constraints: BoxConstraints(
+              maxHeight: 200, maxWidth: 200, minHeight: 100, minWidth: 100),),
+         * 
+         */
+      ),
       title: "Base widget",
     );
   }
