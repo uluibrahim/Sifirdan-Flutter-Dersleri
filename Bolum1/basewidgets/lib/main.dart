2a903ac13a6cd870c38f5c33be5690cfa9d5c228
uluibrahim <iulu17@posta.pau.edu.tr>
2021-06-24 14:58:51 +0300

    MaterialApp ve Scaffold

--- Bolum1/basewidgets/lib/main.dart
+++ Bolum1/basewidgets/lib/main.dart
@@ -5,6 +5,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
- 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Appbar"), // appbar a text basar
+          backgroundColor: Colors.orange, // appbarın arka plan rengi
+        ),
+      ),
+      title: "Base widget",
+    );
+  }
 }
-
