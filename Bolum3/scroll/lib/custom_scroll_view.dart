2b080ad6b04a565d4f8b6b9702d7d092cd84436a
uluibrahim <iulu17@posta.pau.edu.tr>
2021-06-26 15:50:37 +0300

    sliverAppBar kullanımı

--- Bolum3/scroll/lib/custom_scroll_view.dart
+++ Bolum3/scroll/lib/custom_scroll_view.dart
@@ -6,13 +6,73 @@ class CustomScrollViewKullanimi extends StatelessWidget {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          leading: Icon(Icons.real_estate_agent_rounded),
+          leading: Icon(
+            Icons.real_estate_agent_rounded,
+            size: 35,
+          ),
           backgroundColor: Colors.red,
-          title: Text("Title"),
+          title: Text(
+            "Sliver App Bar",
+            style: TextStyle(fontSize: 35),
+          ),
           expandedHeight: 200,
           floating: true,
+          pinned: true,
+          flexibleSpace: Image.network(
+            "https://picsum.photos/seed/picsum/200/300",
+            fit: BoxFit.cover,
+          ),
         ),
+        SliverList(delegate: SliverChildListDelegate(sliverListesi))
       ],
     );
   }
+
+  List<Widget> get sliverListesi => [
+        Container(
+          alignment: Alignment.center,
+          height: 150,
+          color: Colors.cyan,
+          child: Text(
+            "Sliver liste eleamanı",
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          height: 150,
+          color: Colors.blueGrey,
+          child: Text(
+            "Sliver liste eleamanı",
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          height: 150,
+          color: Colors.brown,
+          child: Text(
+            "Sliver liste eleamanı",
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          height: 150,
+          color: Colors.teal,
+          child: Text(
+            "Sliver liste eleamanı",
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          height: 150,
+          color: Colors.orange,
+          child: Text(
+            "Sliver liste eleamanı",
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
+        ),
+      ];
 }
