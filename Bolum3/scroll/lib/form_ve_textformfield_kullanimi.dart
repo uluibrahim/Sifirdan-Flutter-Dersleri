6b908ebd47b02435dbddaf89e68e892868142ff0
uluibrahim <iulu17@posta.pau.edu.tr>
2021-06-29 17:38:23 +0300

    form key ve otomatik validatemode kullanımı

--- Bolum3/scroll/lib/form_ve_textformfield_kullanimi.dart
+++ Bolum3/scroll/lib/form_ve_textformfield_kullanimi.dart
@@ -6,6 +6,9 @@ class TextFormFieldKullanimi extends StatefulWidget {
 }
 
 class _TextFormFieldKullanimiState extends State<TextFormFieldKullanimi> {
+  String _ad, _email, _password;
+  AutovalidateMode autoControl = AutovalidateMode.disabled;
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -28,9 +31,34 @@ class _TextFormFieldKullanimiState extends State<TextFormFieldKullanimi> {
   }
 
   Widget buildBody() {
-    return Padding(
-      padding: EdgeInsets.all(20),
-      child: buildListView(),
+    return Column(
+      children: [
+        Flexible(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Form(
+              key: formKey,
+              //autovalidate: true,
+              autovalidateMode: autoControl,
+              child: buildListView(),
+            ),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: _girisBilgileriniOnayla,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              "Kaydet",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5
+                  .copyWith(color: Colors.white),
+            ),
+          ),
+        ),
+        SizedBox(height: 20),
+      ],
     );
   }
 
@@ -45,6 +73,14 @@ class _TextFormFieldKullanimiState extends State<TextFormFieldKullanimi> {
             border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(10))),
           ),
+          validator: (String value) {
+            if (value.length < 6) {
+              return "Hatalı giriş";
+            } else {
+              return null;
+            }
+          },
+          onSaved: (value) => _ad = value,
         ),
         SizedBox(height: 10),
         TextFormField(
@@ -56,6 +92,14 @@ class _TextFormFieldKullanimiState extends State<TextFormFieldKullanimi> {
             border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(10))),
           ),
+          validator: (value) {
+            if (!value.contains("@")) {
+              return "Hata";
+            } else {
+              return null;
+            }
+          },
+          onSaved: (value) => _email = value,
         ),
         SizedBox(height: 10),
         TextFormField(
@@ -68,9 +112,27 @@ class _TextFormFieldKullanimiState extends State<TextFormFieldKullanimi> {
             border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(10))),
           ),
+          validator: (value) {
+            if (value.length < 6) {
+              return "Hatalı";
+            } else {
+              return null;
+            }
+          },
+          onSaved: (value) => _password = value,
         ),
       ],
     );
+  }
+
+  void _girisBilgileriniOnayla() {
+    if (formKey.currentState.validate()) {
+      formKey.currentState.save();
+      print("ad: $_ad Email: $_email Şifre $_password");
+    }
+    setState(() {
+      autoControl = AutovalidateMode.onUserInteraction;
+    });
   }
 
   AppBar appBar() => AppBar(title: Text("Text Form Field"));
