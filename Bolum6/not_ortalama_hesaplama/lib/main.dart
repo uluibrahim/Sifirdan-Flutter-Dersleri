606cf9c5da9e9c8cda2a55739af7811ebf365bc9
uluibrahim <iulu17@posta.pau.edu.tr>
2021-07-01 11:29:52 +0300

    genel yapı oluşturuldu

--- Bolum6/not_ortalama_hesaplama/lib/main.dart
+++ Bolum6/not_ortalama_hesaplama/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,9 +29,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double? harfDegeri = 4;
+  int? dersKredisi = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset:
+          false, // klavye açıldığında ekran daralmasını önler
       appBar: appBar(),
       body: buildBody(),
     );
@@ -37,33 +43,82 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget buildBody() {
     return Container(
-      color: Colors.grey,
+      color: Colors.deepOrange.shade100,
       child: Column(
         children: [
           Expanded(
             // STATIC FORM TUTAN CONTAİNER
             child: Container(
               padding: EdgeInsets.all(10),
-              color: Colors.white10,
+              color: Colors.teal.shade100,
               child: Form(
                 child: Column(
                   children: [
                     TextFormField(
-                        decoration: InputDecoration(
-                          hintText: "Ders Adı",
-                          border: OutlineInputBorder(),
+                      decoration: InputDecoration(
+                        hintText: "Ders Adı",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
                         ),
-                        validator: (value) {
-                          if (value!.length < 1) {
-                            return "Ders boş bırakılamaz";
-                          } else {
-                            return null;
-                          }
-                        },
-                        onSaved: (value) {}),
-                    Expanded(
-                      child: Row(children: []),
+                      ),
+                      validator: (value) {
+                        if (value!.length < 1) {
+                          return "Ders boş bırakılamaz";
+                        } else {
+                          return null;
+                        }
+                      },
+                      onSaved: (value) {},
                     ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          child: DropdownButton<int>(
+                            hint: Text("Kredi 1"),
+                            items: buildKrediListesi(),
+                            value: dersKredisi!,
+                            onChanged: (value) {
+                              setState(() {
+                                dersKredisi = value!;
+                              });
+                            },
+                          ),
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                          margin: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 2, color: Colors.indigo),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: DropdownButton<double>(
+                            hint: Text("AA"),
+                            items: buildHarfNotuListesi(),
+                            value: harfDegeri,
+                            onChanged: (value) {
+                              setState(() {
+                                harfDegeri = value;
+                              });
+                            },
+                          ),
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 30, vertical: 0),
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 2, color: Colors.indigo),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    divider(),
                   ],
                 ),
               ),
@@ -80,5 +135,44 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Divider divider() {
+    return Divider(
+      thickness: 1.5,
+      color: Colors.red,
+      indent: 40,
+      endIndent: 40,
+    );
+  }
+
   AppBar appBar() => AppBar(title: Text("Not Ortama Hesapla"));
+}
+
+List<DropdownMenuItem<double>> buildHarfNotuListesi() {
+  List<DropdownMenuItem<double>> harfNotuListesi = [];
+
+  harfNotuListesi.add(DropdownMenuItem(child: Text("AA"), value: 4));
+  harfNotuListesi.add(DropdownMenuItem(child: Text("AB"), value: 3.5));
+  harfNotuListesi.add(DropdownMenuItem(child: Text("BB"), value: 3));
+  harfNotuListesi.add(DropdownMenuItem(child: Text("BC"), value: 2.5));
+  harfNotuListesi.add(DropdownMenuItem(child: Text("CC"), value: 2));
+  harfNotuListesi.add(DropdownMenuItem(child: Text("CD"), value: 1.5));
+  harfNotuListesi.add(DropdownMenuItem(child: Text("DD"), value: 1));
+  harfNotuListesi.add(DropdownMenuItem(child: Text("FF"), value: 0));
+
+  return harfNotuListesi;
+}
+
+List<DropdownMenuItem<int>> buildKrediListesi() {
+  List<DropdownMenuItem<int>> krediler = [];
+
+  for (var i = 1; i <= 10; i++) {
+    krediler.add(
+      DropdownMenuItem(
+        child: Text("Kredi $i"),
+        value: i,
+      ),
+    );
+  }
+
+  return krediler;
 }
